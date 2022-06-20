7d34b8db753692c50fcb7a3623c54d8c28685ad1
Guruprasad Kulkarni <guru@linux.com>
2022-06-21 00:15:51 +0200

    added gradle cache for github actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,3 +11,30 @@ pluginManagement {
     }
 }
 rootProject.name="nokri"
+
+
+plugins {
+    id("com.gradle.enterprise") version("3.10.1") // https://plugins.gradle.org/plugin/com.gradle.enterprise
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        capture {
+            isTestLogging = true
+            isBuildLogging = true
+            isTaskInputFiles = true
+        }
+        publishAlways()
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+        directory = File(rootDir, "build-cache")
+        removeUnusedEntriesAfterDays = 30
+        logger.info("Enabled Build Cache and Storing it in $rootDir/build-cache")
+    }
+}
